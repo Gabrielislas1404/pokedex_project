0d97982f20ca7666fceae668145fb8011e701c6e
Gabriel Islas <gabrielislas10@hotmail.com>
2024-10-28 22:00:48 -0600

    jsx desing ready

---
+++ src/components/Main.js
@@ -0,0 +1,107 @@
+function Main() {
+  return (
+    <section className="pokedex">
+      <div className="pokedex__left-side">
+        <div className="pokedex__logo"></div>
+        <div className="pokedex__bg-curve1_left"></div>
+        <div className="pokedex__bg_curve2_left"></div>
+        <div className="pokedex__curve1_left">
+          <div className="pokedex__buttonGlass">
+            <div className="pokedex__reflect"> </div>
+          </div>
+          <div className="pokedex__miniButtonGlass_red"></div>
+          <div className="pokedex__miniButtonGlass_yellow"></div>
+          <div className="pokedex__miniButtonGlass_green"></div>
+        </div>
+        <div className="pokedex__curve2_left">
+          <div className="pokedex__junction">
+            <div className="pokedex__junction_up"></div>
+            <div className="pokedex__junction_down"></div>
+          </div>
+        </div>
+        <div className="pokedex__screen">
+          <div className="pokedex__top-picture">
+            <div className="pokedex__buttontop-picture_right"></div>
+            <div className="pokedex__buttontop-picture_left"></div>
+          </div>
+          <div className="pokedex__picture">
+            <img
+              className="pokedex__picture-img"
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/200653/psykokwak.gif"
+              alt="psykokwak"
+              height="170"
+            />
+          </div>
+          <div className="pokedex__buttonbottom-picture"></div>
+          <div className="pokedex__speakers">
+            <div className="pokedex__sp"></div>
+            <div className="pokedex__sp"></div>
+            <div className="pokedex__sp"></div>
+            <div className="pokedex__sp"></div>
+          </div>
+        </div>
+        <div className="pokedex__big-bluebutton"></div>
+        <div className="pokedex__barbutton_left"></div>
+        <div className="pokedex__barbutton_right"></div>
+        <div className="pokedex__cross">
+          <div className="pokedex__leftcross">
+            <div className="pokedex__leftT"></div>
+          </div>
+          <div className="pokedex__topcross">
+            <div className="pokedex__upT"></div>
+          </div>
+          <div className="pokedex__rightcross">
+            <div className="pokedex__rightT"></div>
+          </div>
+          <div className="pokedex__midcross">
+            <div className="pokedex__midCircle"></div>
+          </div>
+          <div className="pokedex__botcross">
+            <div className="pokedex__downT"></div>
+          </div>
+        </div>
+      </div>
+      <div className="pokedex__right-side">
+        <div className="pokedex__stats">
+          <strong>Name:</strong> Psyduck
+          <br />
+          <strong>Type:</strong> Water
+          <br />
+          <strong>Height:</strong> 2'072''
+          <br />
+          <strong>Weight:</strong> 43.2 lbs.
+          <br />
+          <br />
+          <strong>The duck Pokemon</strong>
+          <br />
+          Uses mysterious powers to perform various attacks.
+        </div>
+        <div className="pokedex__blueButtons_up">
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+        </div>
+        <div className="pokedex__blueButtons_down">
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+          <div className="pokedex__blueButton"></div>
+        </div>
+        <div className="pokedex__miniButtonGlass_orange"></div>
+        <div className="pokedex__miniButtonGlass_greeny"></div>
+        <div className="pokedex__bar-button_left"></div>
+        <div className="pokedex__bar-button_right"></div>
+        <div className="pokedex__yellowBox_left"></div>
+        <div className="pokedex__yellowBox_right"></div>
+        <div className="pokedex__bg-curve1_right"></div>
+        <div className="pokedex__bg-curve2_right"></div>
+        <div className="pokedex__curve1_right"></div>
+        <div className="pokedex__curve2_right"></div>
+      </div>
+    </section>
+  );
+}
+export default Main;
